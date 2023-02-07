7d6b1cde75c28b51c11dee807cb8fe652b9b0a2c
Moeid Haider <moeidhaider9@gmail.com>
2023-02-07 12:06:09 +0000

    First Commit

---
+++ hello_web_project/app.rb
@@ -0,0 +1,36 @@
+# file: app.rb
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+
+  # Declares a route that responds to a request with:
+  #  - a GET method
+  #  - the path /
+  get '/names' do
+    # The code here is executed when a request is received and we need to 
+    # send a response. 
+    names = params[:names]
+    # We can return a string which will be used as the response content.
+    # Unless specified, the response status code will be 200 (OK).
+    return "#{names}"
+  end
+
+  post '/submit' do
+    name = params[:name] # The value is 'Alice'
+    message = params[:message]
+    # Do something with `name`...
+  
+    return "Thanks #{name} you sent this message: #{message}"
+  end
+
+  post '/sort-names' do
+    names = params[:names] #names = Joe, Alice, Zoe, Julia, Kieran
+		
+
+		return names.split(",").sort.join(",")
+
+	end
+end
---
+++ hello_web_project/spec/integration/app_spec.rb
@@ -0,0 +1,37 @@
+# file: spec/integration/application_spec.rb
+
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+  context "GET to /names" do
+    it "returns 200 OK with the right content" do
+      # Send a GET request to /
+      # and returns a response object we can test.
+      response = get("/names?names=Julia, Mary, Karim")
+
+      # Assert the response status code and body.
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('Julia, Mary, Karim')
+    end
+  end
+  context "POST/" do
+    it 'returns 200 OK with right content' do
+        response = post("/sort-names?names= Joe, Alice, Zoe, Julia, Kieran")
+
+        expect(response.status).to eq(200)
+        expect(response.body).to eq(' Alice, Joe, Julia, Kieran, Zoe')
+    end
+  end
+
+end
+
+
